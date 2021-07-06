408cd4c2281e9f623a8a3f1eb608a79e24764f77
reeksts <karlis.rieksts@gmail.com>
2021-07-06 15:17:23 +0200

    testing

--- 01_SSR_model_development/main.cpp
+++ 01_SSR_model_development/main.cpp
@@ -10,7 +10,7 @@
 
 #include "SSR_model.h"
 
-//TESTING 222
+//TESTING 333
 
 void load_all_data_sets(std::vector<std::vector<std::string>> &full_testsite_filename_vector) {
     std::string file_location_1 {"01_SAAR_original_data_set\\"};                     // add files here
